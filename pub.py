98a2846ee551a9b2abc2f28eeee05d5af48c071d
MaherHassanain <Maher_Hassanain@hotmail.com>
2020-02-23 14:20:18 -0500

    Update pub.py

--- pub.py
+++ pub.py
@@ -3,14 +3,7 @@ import paho.mqtt.client as mqtt
 client = mqtt.Client()
 client.connect('localhost',1883) #as Broker
 
-def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        print("connected OK Returned code= ",rc)
-        client.subscribe("topic/test")
-    else:
-        print("Bad connection Returned code=",rc)
 
 
 while True:
-    client.on_connect = on_connect
     client.publish("topic/test", input('Message : '))
